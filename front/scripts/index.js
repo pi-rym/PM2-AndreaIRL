b7aa0bc04b95ccb933bab0e19b8d81d5137cd838
Andrea Rojas <anivro53@hotmail.com>
2024-03-17 05:05:25 -0600

    Mapeo

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,16 @@
+tempData.map((item)=>{
+        const genero=item.genre
+const html = `<div class="col-md-4">
+                <div class="card mb-3">
+                <img src="${item.poster}" class="card-img-top h-60" alt="${item.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${item.title}</h5>
+                    <p class="card-text">${genero.join()}</p>
+                    <p class="card-text"><small class="text-muted">director ${item.director}</small></p>
+                </div>
+                </div>
+            </div>`
+    console.log(html)
+    document.getElementById('pelis').innerHTML+=html
+})
 console.log(tempData);
