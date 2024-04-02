19473d63464c8eaa9a60ddb3024eac0d71e18472
Andrea Rojas <anivro53@hotmail.com>
2024-04-02 16:42:41 -0600

    se revisa el documento

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -5,17 +5,16 @@ function renderFilms(movie){
     movieElement.classList.add('movie')
     containerMovie.classList.add('divMovie')
 
-    movieElement.innerHTML =`<img src="${movie.poster}" alt="${movie.title}">
+    movieElement.innerHTML =`<img src="${movie.poster}" alt="${movie.title}"/>
     `
     containerMovie.innerHTML = `
     <h3>${movie.title} (${movie.year})</h3>
     <p><strong>Director:</strong> ${movie.director}</p>
     <p><strong>Duración:</strong> ${movie.duration}</p>
-    <p><strong>Género:</strong> ${movie.genre.join(',')}</p>
+    <p><strong>Género:</strong> ${movie.genre.join(', ')}</p>
     <p><strong>Rate:</strong> ${movie.rate}</p>
     `
     filmsSection.appendChild(movieElement)
     movieElement.appendChild(containerMovie)
 }
-tempData.forEach(renderFilms)
-console.log(tempData);
+$.get('https://students-api.up.railway.app/movies', (data)=> data.forEach(renderFilms))
